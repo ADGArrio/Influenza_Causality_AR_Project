675b97b65b839393bfae000c94726687d7262faf
Dylan Setiawan <dylangsetiawan@gmail.com>
2025-11-29 15:19:13 -0500

    concretized datatypes to maximize modularity and added basic OLS structure functions

--- application/functions.go
+++ application/functions.go
@@ -1,3 +1,24 @@
 package main
 
-// can have parallel processing if needed to fit multiple models
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
+// --- FUNCTIONS FOR BASE ---
+// Returns current Model Spec
+func (rf *ReducedFormVAR) Spec() ModelSpec { return rf.Model }
+
+// Returns coefficient matrices
+func (rf *ReducedFormVAR) Phi() []*mat.Dense { return rf.A }
+
+// Returns error covariance matrix
+func (rf *ReducedFormVAR) CovU() *mat.SymDense { return rf.SigmaU }
+
+// --- OLS IMPLEMENTATION ---
+func (e *OLSEstimator) Estimate(ts *TimeSeries, spec ModelSpec, opts EstimationOptions) (*ReducedFormVAR, error) {
+	// 1. Build lagged design matrix X and Y
+	// 2. Run (possibly multiple) linear regressions
+	// 3. Assemble Phi, C, SigmaU
+	// 4. Return &ReducedFormVAR{...}
+	return nil, nil
+}
